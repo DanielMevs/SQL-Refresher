c324e0cd3b67c98ffe7fadb2b7b2efbc41423678
DanielMevs <dannymevs@hotmail.com>
2024-02-11 13:21:03 -0500

    completed count and distinct sections

---
+++ sql_statement_fundamentals/select/count/count.sql
@@ -0,0 +1,2 @@
+-- SELECT COUNT(DISTINCT(rating)) from film;
+SELECT COUNT(DISTINCT amount) FROM payment;
---
+++ sql_statement_fundamentals/select/distinct/distinct.sql
@@ -0,0 +1,3 @@
+-- SELECT DISTINCT(release_year) FROM film;
+-- SELECT DISTINCT(rental_rate) FROM film;
+SELECT DISTINCT(rating) FROM film;
