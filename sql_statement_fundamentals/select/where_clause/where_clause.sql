4f8c4e6f7f2f7e07e516c364f17e2e52fc3fba70
DanielMevs <dannymevs@hotmail.com>
2024-02-12 19:03:11 -0500

    completed where clause section

---
+++ sql_statement_fundamentals/select/where_clause/where_clause.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM customer
+-- WHERE first_name = 'Jared';
+-- SELECT COUNT(title) FROM film
+-- WHERE rental_rate > 4 AND replacement_cost >= 19.99
+-- AND rating='R';
+-- SELECT COUNT(*) FROM film
+-- WHERE rating = 'R' OR rating = 'PG-13';
+-- SELECT email FROM customer
+-- WHERE first_name = 'Nancy' and last_name='Thomas';
+-- SELECT description FROM film
+-- WHERE title='Outlaw Hanky';
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
