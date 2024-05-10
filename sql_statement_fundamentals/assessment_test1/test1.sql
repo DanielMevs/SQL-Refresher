fd005fd34bff5fe79d16b82604f24804e861cb7e
DanielMevs <dannymevs@hotmail.com>
2024-05-09 20:01:06 -0400

    completed assessment test 1

---
+++ sql_statement_fundamentals/assessment_test1/test1.sql
@@ -0,0 +1,28 @@
+-- 1. Return the customer IDs of customers
+-- who have spent at least $110 with the
+-- staff member who has an ID of 2.
+
+SELECT customer_id
+FROM payment
+WHERE staff_id=2
+GROUP BY customer_id
+HAVING SUM(amount) >= 110;
+
+-- 2. How many films begin with the letter
+-- J?
+
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'J%';
+
+-- 3. What customer has the highest 
+-- customer ID number whose name 
+-- starts with an 'E' and has
+-- an address ID lower than 500?
+
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE 'E%'
+AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1;
