f6ff9513979fdf86308ea4b2759fa853e9371712
DanielMevs <dannymevs@hotmail.com>
2024-06-01 14:57:22 -0400

    completed exporting and import csv files in pg admin section

---
+++ conditional_expressions_and_procedures/import_and_export/data.sql
@@ -0,0 +1,16 @@
+-- Importing a csv file does not
+-- automatically create a table
+-- for you. You must create a table manually
+CREATE TABLE simple(
+	a INTEGER,
+	b INTEGER,
+	c INTEGER
+)
+
+-- Navigate to your table in PGadmin
+-- right click the table
+-- select Import/Export
+-- Choose the csv file you wish to import
+-- or type in the path you want to
+-- export your table as a csv file to.
+
